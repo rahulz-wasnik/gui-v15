8ad52a1c4607f16a8f4a70b14ad0f335fdafb460
Rahul Wasnik <rahulz.wasnik@gmail.com>
2023-03-24 15:24:31 +0530

    Updating the workspace layout flag and creating angular app and lib

---
+++ apps/app-a/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gui-v15-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app-a';
+}
